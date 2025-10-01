c8f3b9ed1267ea97c8b4a26966e7a1f600fc736f
Mark <mark.harrop@airelogic.com>
2025-10-01 13:59:44 +0100

    Add embedded NATS

--- Makefile
+++ Makefile
@@ -1,6 +1,18 @@
 watch:
 	templ generate --watch --proxy="http://localhost:3000" --cmd="go run ." --open-browser=false
 
+# Generate templ files once for debugging
+gen:
+	templ generate
+
+# Watch templ files without running the server (for debugging)
+watch-templ:
+	templ generate --watch
+
+# Debug mode - generate once then run with delve
+debug: gen
+	dlv debug --headless --listen=:2345 --api-version=2 --accept-multiclient
+
 test:
 	go test ./...
 
--- main.go
+++ main.go
@@ -4,14 +4,58 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
+
+	"github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
 )
 
 func main() {
 
 	const port = 3000
+	opts := &server.Options{
+		Host:   "127.0.0.1",
+		Port:   4222, // Default NATS port
+		NoLog:  false,
+		NoSigs: true,
+	}
+
+	ns, err := server.NewServer(opts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Start the server
+	go ns.Start()
+
+	// Wait for server to be ready for connections
+	if !ns.ReadyForConnections(4 * time.Second) {
+		panic("not ready for connection")
+	}
+
+	nc, err := nats.Connect(ns.ClientURL())
+
+	subject := "my-subject"
+
+	if err != nil {
+		panic(err)
+	}
+
+	nc.Subscribe(subject, func(msg *nats.Msg) {
+		// Print message data
+		data := string(msg.Data)
+		fmt.Println(data)
+
+		// Shutdown the server (optional)
+		// ns.Shutdown()
+	})
+
+	// Publish data to the subject
+	nc.Publish(subject, []byte("Hello embedded NATS!"))
 
 	r := chi.NewRouter()
 	homePage := home("Vanilla Go")
